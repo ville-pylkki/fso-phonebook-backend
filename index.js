b804a230af31f0d09f4b61e2c3a2b44ee02b57a7
Ville Pylkki <ville.pylkki@iki.fi>
2024-01-19 12:43:15 +0200

    p3ex3.10 Changes application port to be read from environmental variables

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.get('/info', (request, response) => {
 	)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.info(`Server running on port ${PORT}`)
 })
